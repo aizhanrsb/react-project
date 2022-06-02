eacec91baa6dcec4ed124f28d7d4ddbd5aac526f
aizhanrsb <rysbekovaaizhan@gmail.com>
2022-06-02 21:15:51 +0600

    added filters/pagination

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,44 +1,63 @@
-import { Button, Card, CardActions, CardContent, CardMedia, Typography } from '@mui/material';
-import React from 'react';
-import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
-import InfoIcon from '@mui/icons-material/Info';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useNavigate } from 'react-router-dom';
-import {useContext} from "react"
-import {productsContext} from "../../contexts/productsContext"
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import InfoIcon from "@mui/icons-material/Info";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { productsContext } from "../../contexts/productsContext";
 
-
-const ProductCard = ({item}) => {
+const ProductCard = ({ item }) => {
   const navigate = useNavigate();
-  const {deleteProduct} = useContext(productsContext)
-    console.log(item);
-    return (
-        <Card sx={{ maxWidth: 345, margin: "10px" }}>
-        <CardMedia
-          component="img"
-          alt="green iguana"
-          height="200"
-          image={item.image}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {item.title}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-           {item.description.length >20 ? `&{item.description.slice(0,20)}...` : item.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          {/*! icons added as components */}
-        
-          <Button size="small" onClick={()=> deleteProduct(item.id)}><DeleteIcon /></Button>   
-          <Button size="small" onClick={() => navigate(`/edit/${item.id}`)}>< EditIcon /></Button>
-          <Button size="small"><ShoppingCartIcon /></Button>
-          <Button size="small" onClick={() => navigate(`/products/${item.id}`)}><InfoIcon /></Button>
-        </CardActions>
-      </Card>
-    );
+  const { deleteProduct } = useContext(productsContext);
+  // console.log(item);
+  return (
+    <Card sx={{ maxWidth: 345, margin: "10px" }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="200"
+        image={item.image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {item.description.length > 20
+            ? `${item.description.slice(0, 20)}...`
+            : item.description}
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div">
+          {item.price}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {/*! icons added as components */}
+
+        <Button size="small" onClick={() => deleteProduct(item.id)}>
+          <DeleteIcon />
+        </Button>
+        <Button size="small" onClick={() => navigate(`/edit/${item.id}`)}>
+          <EditIcon />
+        </Button>
+        <Button size="small">
+          <ShoppingCartIcon />
+        </Button>
+        <Button size="small" onClick={() => navigate(`/products/${item.id}`)}>
+          <InfoIcon />
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default ProductCard;
